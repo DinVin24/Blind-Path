8b1037ce4186c6bd2f887d09f8352bc36e297288
DinVin24 <67893273+DinVin24@users.noreply.github.com>
2025-02-27 09:32:09 +0000

    uhh

---
+++ Zid.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The player and enemies can't go through this Actor.
+ * 
+ * @author Soltuzu Emanuel & Mandache Alexandru Nutu
+ * @version 22.02.2021
+ */
+public class Zid extends Wall
+{
+    /**
+     * Act - do whatever the Zid wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
